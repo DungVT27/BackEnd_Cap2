6528d194eeec9c1bad586154e3a1075ead5cc6c0
DungVT27 <dungvt@runsystem.net>
2023-05-10 22:04:44 +0700

    Add id to homepage tour

--- BackEnd/app/Http/Resources/HomepageGroupResource.php
+++ BackEnd/app/Http/Resources/HomepageGroupResource.php
@@ -16,6 +16,7 @@ class HomepageGroupResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'from_date' => $this->from_date,
             'to_date' => $this->to_date,
--- BackEnd/app/Http/Resources/HomepageToursResource.php
+++ BackEnd/app/Http/Resources/HomepageToursResource.php
@@ -15,6 +15,7 @@ class HomepageToursResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'address' => $this->address,
             'from_date' => $this->from_date,
